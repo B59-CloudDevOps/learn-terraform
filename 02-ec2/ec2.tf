f431b5a5cf78c56b7bea2d092f09c5ba5433f6ed
CodingManoj <CodingManoj@gmail.com>
2025-03-26 22:03:16 -0400

    Terraform Basics : Auto Push

--- 02-ec2/ec2.tf
+++ 02-ec2/ec2.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "xyz" {
+resource "aws_instance" "main" {
   # ami           = "ami-0fcc78c828f981df2"
   ami           = "ami-072983368f2a6eab5"
   instance_type = "t3.micro"
