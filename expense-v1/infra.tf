ae811163ba2dfa5855301a59cc6db4bc8e76e731
CodingManoj <CodingManoj@gmail.com>
2025-03-27 20:43:40 -0400

    Terraform Basics : Auto Push

--- expense-v1/infra.tf
+++ expense-v1/infra.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "frontend" {
   ami                    = "ami-0fcc78c828f981df2"
   instance_type          = "t3.micro"
-  vpc_security_group_ids = "sg-0b37bb6b6f027ffc5"
+  vpc_security_group_ids = ["sg-0b37bb6b6f027ffc5"]
 
   tags = {
     Name = "frontend"
@@ -11,7 +11,7 @@ resource "aws_instance" "frontend" {
 resource "aws_instance" "backend" {
   ami                    = "ami-0fcc78c828f981df2"
   instance_type          = "t3.micro"
-  vpc_security_group_ids = "sg-0b37bb6b6f027ffc5"
+  vpc_security_group_ids = ["sg-0b37bb6b6f027ffc5"]
 
   tags = {
     Name = "backend"
@@ -21,7 +21,7 @@ resource "aws_instance" "backend" {
 resource "aws_instance" "mysql" {
   ami                    = "ami-0fcc78c828f981df2"
   instance_type          = "t3.micro"
-  vpc_security_group_ids = "sg-0b37bb6b6f027ffc5"
+  vpc_security_group_ids = ["sg-0b37bb6b6f027ffc5"]
 
   tags = {
     Name = "mysql"
