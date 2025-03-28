11975f282e402a133b28a2b09b6f92b9cd0fc242
CodingManoj <CodingManoj@gmail.com>
2025-03-27 21:43:39 -0400

    Terraform Basics : Auto Push

--- 04-functions/vars.tf
+++ 04-functions/vars.tf
@@ -4,3 +4,7 @@ variable "fruits" {}
 output "apple_op" {
   value = var.fruits
 }
+
+output "apple_op_x" {
+  value = "Apple is ${var.fruits["apple"]["color"]} in color and its taste is ${var.fruits["apple"]["taste"]} and its price is ${var.fruits["apple"]["price"]} per ${var.fruits["apple"]["metric"]}"
+}
