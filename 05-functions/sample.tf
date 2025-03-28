e059137537c5ede58d597bda7c241d9e7a70694e
CodingManoj <CodingManoj@gmail.com>
2025-03-27 22:03:40 -0400

    Terraform Basics : Auto Push

---
+++ 05-functions/sample.tf
@@ -0,0 +1,7 @@
+# Declaring an empty variable
+variable "fruits" {}
+
+
+output "apple_op_x" {
+  value = "Apples are ${lookup(var.fruits["apple"], "color", "GREEN")} and their state of origin is ${lookup(var.fruits["apple"], "state", "HP")}"
+}
