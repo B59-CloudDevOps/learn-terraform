2d08c4448558bdcd5bc6e8c96b0dfa36713fce8f
CodingManoj <CodingManoj@gmail.com>
2025-03-30 21:04:31 -0400

    Terraform Basics : Auto Push

--- expense-v3/infra.tf
+++ expense-v3/infra.tf
@@ -15,13 +15,14 @@ variable "componets" {
   }
 }
 resource "aws_instance" "main" {
-  count                  = length(var.componets)
-  ami                    = "ami-0fcc78c828f981df2"
-  instance_type          = "t3.micro"
+  for_each = var.componets
+
+  ami                    = each.value["ami"]
+  instance_type          = each.value["instance_type"]
   vpc_security_group_ids = ["sg-0b37bb6b6f027ffc5"]
 
   tags = {
-    Name = var.componets[count.index]
+    Name = each.key
   }
 }
 
