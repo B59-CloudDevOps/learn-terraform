039f535be8c180813ff3b393ef22cbb8b2c029df
CodingManoj <CodingManoj@gmail.com>
2025-03-30 21:03:15 -0400

    Terraform Basics : Auto Push

--- expense-v3/infra.tf
+++ expense-v3/infra.tf
@@ -2,12 +2,15 @@ variable "componets" {
   default = {
     frontend = {
       instance_type = "t3.micro"
+      ami           = "ami-0fcc78c828f981df2"
     }
     backend = {
       instance_type = "t2.micro"
+      ami           = "ami-0fcc78c828f981df2"
     }
     mysql = {
       instance_type = "t3.medium"
+      ami           = "ami-0fcc78c828f981df2"
     }
   }
 }
