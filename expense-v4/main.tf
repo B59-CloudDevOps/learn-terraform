48685f171c4932eaa7bc80d95fc35f7db84483a2
CodingManoj <CodingManoj@gmail.com>
2025-03-31 21:20:29 -0400

    Terraform Basics : Auto Push

--- expense-v4/main.tf
+++ expense-v4/main.tf
@@ -1,7 +1,8 @@
 module "app" {
+  for_each               = var.componets
   source                 = "./app"
-  ami                    = ""
-  instance_type          = ""
-  vpc_security_group_ids = ""
-  name                   = ""
+  ami                    = var.ami
+  instance_type          = each.value["instance_type"]
+  vpc_security_group_ids = var.vpc_security_group_ids
+  name                   = each.key
 }
