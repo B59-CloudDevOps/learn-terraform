7e87325bf1d80aa8fcbe9c4ce3fb5bda53c2d051
CodingManoj <CodingManoj@gmail.com>
2025-03-31 20:50:59 -0400

    Terraform Basics : Auto Push

--- 09-modules/main.tf
+++ 09-modules/main.tf
@@ -1,4 +1,4 @@
 module "sample" {
-  source = "./sample" # Source can be from Terarform Registry Path, Local Path or from a remote git repository
+  source = "./sample-module" # Source can be from Terarform Registry Path, Local Path or from a remote git repository
 }
 
