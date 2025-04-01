9bdb7dbab1c5860e0aed24f5d31c2ed8f900572b
CodingManoj <CodingManoj@gmail.com>
2025-03-31 21:18:28 -0400

    Terraform Basics : Auto Push

--- expense-v4/main.tf
+++ expense-v4/main.tf
@@ -1,14 +1,3 @@
-
-resource "aws_instance" "main" {
-  for_each = var.componets
-
-  ami = each.value["ami"]
-  # instance_type          = each.value["instance_type"] == ".*" ? each.value["instance_type"] : "t3.nano"
-  instance_type          = try(each.value["instance_type"], null) == null ? "t3.nano" : each.value["instance_type"]
-  vpc_security_group_ids = ["sg-0b37bb6b6f027ffc5"]
-
-  tags = {
-    Name = each.key
-  }
+module "app" {
+  source = "./app "
 }
-
