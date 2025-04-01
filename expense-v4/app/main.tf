8af6a063a542e6cf20e3d6563a192e0921491068
CodingManoj <CodingManoj@gmail.com>
2025-03-31 21:26:44 -0400

    Terraform Basics : Auto Push

--- expense-v4/app/main.tf
+++ expense-v4/app/main.tf
@@ -8,3 +8,12 @@ resource "aws_instance" "main" {
     Name = var.name
   }
 }
+
+
+# resource "aws_route53_record" "www" {
+#   zone_id = aws_route53_zone.primary.zone_id
+#   name    = "www.example.com"
+#   type    = "A"
+#   ttl     = 300
+#   records = [aws_eip.lb.public_ip]
+# }
--- expense-v4/main.tf
+++ expense-v4/main.tf
@@ -5,4 +5,5 @@ module "app" {
   instance_type          = each.value["instance_type"]
   vpc_security_group_ids = var.vpc_security_group_ids
   name                   = each.key
+  zone_id                = var.zone_id
 }
