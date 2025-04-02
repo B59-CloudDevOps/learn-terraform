a5cb66058445ded4a107d02a25ad7b26a0353021
CodingManoj <CodingManoj@gmail.com>
2025-04-01 20:54:00 -0400

    Terraform Basics : Auto Push

--- README.md
+++ README.md
@@ -176,3 +176,4 @@ Modules:
         1) Local Provisioner      : When you want some action to be performed on the machine you're running terraform, then we use local provisioner
         2) Remote Provisoner      : When you want some action to be performed on the you're provisioned, then we use remote provisioner
         3) Connection Provisioner : To perform some action on the top of the newly provisoned machine, you need to enable a connection and that can be done via connection-provisioner
+            > Note: Provisioners should always be with in the resource.
--- expense-v4/app/main.tf
+++ expense-v4/app/main.tf
@@ -7,12 +7,14 @@ resource "aws_instance" "main" {
   tags = {
     Name = var.name
   }
-
-  provisioner "local-exec" {
-    command = "sleep 10 ;  cd /home/ec2-user/learn-ansible/; ansible-playbook -i ${self.private_ip},  -e ansible_user=ec2-user -e ansible_password=DevOps321 -e env=dev -e component=${var.name} expense.yaml"
-  }
 }
 
+resource "null_resource" "main" {
+  depends_on = [aws_route53_record.main] # This ensure, provisioner will only be exectued post dns_record creation
+  provisioner "local-exec" {
+    command = "sleep 10 ;  cd /home/ec2-user/learn-ansible/; ansible-playbook -i ${self.private_ip},  -e ansible_user=ec2-user -e ansible_password=DevOps321 -e env=${var.env} -e component=${var.name} expense.yaml"
+  }
+}
 
 resource "aws_route53_record" "main" {
   zone_id = var.zone_id
